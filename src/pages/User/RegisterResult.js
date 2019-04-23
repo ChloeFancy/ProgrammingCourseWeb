30a8af5a3800b5bfecd4dc3b8bbc452a69809538
ChloeFancy <773889885@qq.com>
2019-04-23 12:27:25 +0800

    注册成功页面

--- src/pages/User/RegisterResult.js
+++ src/pages/User/RegisterResult.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { formatMessage, FormattedMessage } from 'umi-plugin-react/locale';
-import { Button } from 'antd';
-import Link from 'umi/link';
+import { Icon } from 'antd';
+import router from 'umi/router';
 import styles from './RegisterResult.less';
 
-const actions = (
-  <div className={styles.actions}>
-    <a href="">
-      <Button size="large" type="primary">
-        <FormattedMessage id="app.register-result.view-mailbox" />
-      </Button>
-    </a>
-    <Link to="/">
-      <Button size="large">
-        <FormattedMessage id="app.register-result.back-home" />
-      </Button>
-    </Link>
-  </div>
-);
+export default class RegisterResult extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 3,
+      timeout: 0,
+    };
+  }
 
-const RegisterResult = ({ location }) => (
-  <div></div>
-);
+  componentDidMount() {
+    const callBack = () => {
+      const { count, timeout } = this.state;
+      if (!count) {
+        clearTimeout(timeout);
+        router.push('/user/login');
+      } else {
+        this.setState({
+          count: count - 1,
+        });
+        this.setState({
+          timeout: setTimeout(callBack, 1000),
+        });
+      }
+    };
+    const timeout = setTimeout(callBack, 1000);
+    this.setState({
+      timeout,
+    });
+  }
 
-export default RegisterResult;
+  render() {
+    const { count } = this.state;
+    return (
+      <div className={styles.registerResult}>
+        <div className={styles.icon}><Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" /></div>
+        <div className={styles.title}>
+          注册成功
+        </div>
+        <div className={styles.title}>
+          {count}秒之后进入登录页
+        </div>
+      </div>
+    );
+  }
+};
