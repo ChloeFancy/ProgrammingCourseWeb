96e1885e3f28c4c2c77904655c0ad944c40049a8
ChloeFancy <773889885@qq.com>
2019-10-18 00:01:04 +0800

    build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.046c1f528492512e6945ce59df0ac4d8.js", "/workbox-v3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.99c8fc34cbc031f8b82a3456c8326766.js", "/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 /* globals workbox */
 /* eslint-disable no-restricted-globals */
